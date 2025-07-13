1d717ed480b2e737f631fdec700f52a15b085d30
imdeepakchahar <imchahardeepak@gmail.com>
2025-07-13 12:37:35 +0530

    cors

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+
+const allowedOrigins = ['https://dev-env.co'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // if you're sending cookies or authorization headers
+}));
 app.use(express.json());
 
 // Routes
